92f6ad6f43633c30164058f4795a3acba204c36d
Daniel Barlow <dan@telent.net>
2017-07-19 00:19:39 +0100

    enshorten shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,6 @@
-with import <nixpkgs> {}; 
-let sourceFilesOnly = path: type:
-    (lib.hasPrefix "var" (toString path)) ||
-    (lib.hasPrefix "target" (toString path)) ;
-in stdenv.mkDerivation {
-    name = "abscissa";
-    src = builtins.filterSource sourceFilesOnly ./.;
-    buildInputs = [ ghc
-      ];
-    shellHook = ''
-      test -f nix-shell-hook.sh && source nix-shell-hook.sh
-    '';
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "haskell-club";
+  version = "0.0.1";
+  buildInputs = [ ghc ];
 }
-
