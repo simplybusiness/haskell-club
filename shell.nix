6c708852a95f6ac3e83fa9b78072c14f4888fe7e
Daniel Barlow <dan@telent.net>
2017-08-02 21:30:09 +0100

    add servant

--- shell.nix
+++ shell.nix
@@ -2,5 +2,8 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "haskell-club";
   version = "0.0.1";
-  buildInputs = [ ghc cabal-install ];
+  buildInputs = [ ghc cabal-install zlib pkgconfig ];
+  shellHook = ''
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${zlib}/lib
+  '';
 }
